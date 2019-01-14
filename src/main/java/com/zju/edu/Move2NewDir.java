b31c6070b27c2c7abed621a9d1bf30e64893fd21
gangliang <gangliang5893@gmail.com>
2019-01-14 09:25:47 +0800

    修改geoserver适量瓦片转移方法，使得请求方式为{z}/{x}/{-y}

---
+++ src/main/java/com/zju/edu/Move2NewDir.java
@@ -0,0 +1,16 @@
+package com.zju.edu;
+
+import util.MoveToNewDir;
+
+import java.io.IOException;
+
+public class Move2NewDir {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("请输入原始文件夹和目标文件夹!");
+        }
+        String sourcePath = args[0];
+        String targetPath = args[1];
+        MoveToNewDir.moveVectorTile(sourcePath, targetPath);
+    }
+}
--- src/test/java/com/zju/edu/ReadVectorTest.java
+++ src/test/java/com/zju/edu/ReadVectorTest.java
@@ -1,18 +1,18 @@
 package com.zju.edu;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import util.MoveToNewDir;
+import util.OracleSingletonEnum;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.zju.edu.ReadVector.getXYZAndContentfromPbf;
@@ -28,23 +28,27 @@ public class ReadVectorTest {
         System.out.println(file.getName().split("\\.")[0].split("_")[0]);
     }
 
-//    @Test
-//    public void getXYZAndContentfromPbf() throws IOException {
-//        String sourcePath="file:///Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk_1234";
-//        String targetPath="file:///Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk_fine";
-//        MoveToNewDir.moveVectorTile(sourcePath,targetPath);
-//    }
+    @Test
+    public void getXYZAndContentfromPbf() throws IOException {
+        String sourcePath = "file:///Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk1";
+        String targetPath = "file:///Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk1_fine_normal";
+        MoveToNewDir.moveVectorTile(sourcePath, targetPath);
+    }
     @Test
     public void testList() throws IOException {
         String first="file:///Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk1/EPSG_4490_08/13_05/0419_0177.pbf";
         String second="file:///Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/tmp/fir.pbf";
         //Files.move(Paths.get(URI.create(first)),Paths.get(URI.create(second)));
         Files.delete(Paths.get(URI.create(second)));
+        OracleSingletonEnum.INSTANCE.getInstance();
     }
 
-    @Test
-    public void getXYZAndContentfromPbf2() throws IOException {
-        getXYZAndContentfromPbf("/Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk_fine/6/105/44.pbf");
-    }
+//    @Test
+//    public void getXYZAndContentfromPbf2() throws IOException {
+//        getXYZAndContentfromPbf("/Users/LG/Documents/pratice/geoserver-2.13.4/data_dir/gwc/vector_tiles_lk_fine/6/105/44.pbf");
+//    }
+
+
+
 
 }
